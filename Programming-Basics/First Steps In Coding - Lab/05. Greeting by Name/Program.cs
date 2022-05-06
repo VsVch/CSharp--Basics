e369e425a4bf71b2e59990ce74ba7fc08ff9c7dd
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-07 01:24:15 +0300

    Add files via upload

--- Programming-Basics/First Steps In Coding - Lab/05. Greeting by Name/Program.cs
+++ Programming-Basics/First Steps In Coding - Lab/05. Greeting by Name/Program.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace _05._Greeting_by_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
