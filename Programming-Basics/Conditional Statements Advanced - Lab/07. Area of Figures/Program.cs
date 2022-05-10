80b18624d43bd6616dd9a9c42add4ad9d51a3bf5
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-10 18:41:33 +0300

    Update Program.cs

--- Programming-Basics/Conditional Statements Advanced - Lab/07. Area of Figures/Program.cs
+++ Programming-Basics/Conditional Statements Advanced - Lab/07. Area of Figures/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+using System;
 
-namespace _07._Area_of_Figures
+namespace AreaOfFigures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a = Console.ReadLine();
+            double s = 0.00;
+            if (a == "square")
+            {
+                double i = double.Parse(Console.ReadLine());
+                s = i * i;
+                Console.WriteLine("{0:F3}",s);
+            }
+            else if (a == "rectangle")
+            {
+                double i1 = double.Parse(Console.ReadLine());
+                double i2 = double.Parse(Console.ReadLine());
+                s = i1 * i2;
+                Console.WriteLine("{0:F3}", s);
+            }
+            else if (a == "circle")
+            {
+                double i = double.Parse(Console.ReadLine());
+                s = i * i* Math.PI;
+                Console.WriteLine("{0:F3}", s);
+            }
+            else if (a == "triangle")
+            {
+                double i1 = double.Parse(Console.ReadLine());
+                double i2 = double.Parse(Console.ReadLine());
+                s = (i1 * i2/2);
+                Console.WriteLine("{0:F3}", s);
+            }
+
         }
     }
 }
