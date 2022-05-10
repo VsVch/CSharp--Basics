427706d1d6c6a76ecfae3a3f81918c95ed83f89e
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-10 18:40:59 +0300

    Update Program.cs

--- Programming-Basics/Conditional Statements Advanced - Lab/06. Speed Info/Program.cs
+++ Programming-Basics/Conditional Statements Advanced - Lab/06. Speed Info/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace _06._Speed_Info
 {
@@ -6,7 +6,35 @@ namespace _06._Speed_Info
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            double speed = double.Parse(Console.ReadLine());
+
+
+            if (speed <=10)
+            {
+                Console.WriteLine($"slow");
+            }
+            else if (speed>10&&speed<=50)
+            {
+                Console.WriteLine($"average");
+            }
+            else if (speed > 50 && speed <= 150)
+            {
+                Console.WriteLine($"fast");
+            }
+            else if (speed > 150 && speed <= 1000)
+            {
+                Console.WriteLine($"ultra fast");
+            }
+            else if (speed > 1000)
+            {
+                Console.WriteLine($"extremely fast");
+            }
+            
+
+
+
         }
     }
 }
