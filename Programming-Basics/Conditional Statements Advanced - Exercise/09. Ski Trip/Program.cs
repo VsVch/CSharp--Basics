036a9a3220cfa648b6ad4342b0b3ffa6d3062af9
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-11 19:08:17 +0300

    Add files via upload

--- Programming-Basics/Conditional Statements Advanced - Exercise/09. Ski Trip/Program.cs
+++ Programming-Basics/Conditional Statements Advanced - Exercise/09. Ski Trip/Program.cs
@@ -1,1 +1,12 @@
+﻿using System;
 
+namespace _09._Ski_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
