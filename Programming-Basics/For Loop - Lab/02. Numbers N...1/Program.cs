c6cff8140af11bdebdbc3dccb3859e9d42cddd9a
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-12 18:42:34 +0300

    Update Program.cs

--- Programming-Basics/For Loop - Lab/02. Numbers N...1/Program.cs
+++ Programming-Basics/For Loop - Lab/02. Numbers N...1/Program.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = num; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
