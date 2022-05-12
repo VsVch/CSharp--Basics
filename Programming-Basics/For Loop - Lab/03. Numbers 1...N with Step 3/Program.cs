3e812d82e5d0d75519c14b6861b83148873a5914
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-12 18:45:20 +0300

    Update Program.cs

--- Programming-Basics/For Loop - Lab/03. Numbers 1...N with Step 3/Program.cs
+++ Programming-Basics/For Loop - Lab/03. Numbers 1...N with Step 3/Program.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
