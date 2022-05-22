f25759d6271259c6b9b8373b6fa111777fee45a3
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-22 06:47:41 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/06. Name Game/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/06. Name Game/Program.cs
@@ -1,1 +1,49 @@
+﻿using System;
 
+namespace _06._Name_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string name = string.Empty;
+
+            string winName = string.Empty;
+            double maxPoints = 0;
+            while ("Stop" != (name = Console.ReadLine()))
+            {
+                double points = 0;
+                for (int i = 0; i < name.Length; i++)
+                {
+                    int num = int.Parse(Console.ReadLine());
+
+
+                    if (num == name[i])
+                    {
+                        points += 10;
+                    }
+                    else
+                    {
+                        points += 2;
+                    }
+
+                    if (maxPoints <= points)
+                    {
+                        winName = name;
+                        maxPoints = points;
+                    }
+
+
+                }
+
+
+            }
+
+            Console.WriteLine($"The winner is {winName} with {maxPoints} points!");
+
+
+        }
+    }
+}
