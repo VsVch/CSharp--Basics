c37e5db3dd5d628860c7d68a9bc6f58e133ae435
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-22 06:42:40 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/03. Coffee Machine/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/03. Coffee Machine/Program.cs
@@ -1,1 +1,93 @@
+﻿using System;
 
+namespace _03._Coffee_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string napitka = Console.ReadLine();
+            string sugar = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (napitka)
+            {
+                    case "Espresso":
+                    if (sugar == "Without")
+                    {
+                        price = 0.9;
+                    }
+                    else if (sugar == "Normal")
+                    {
+                        price = 1;
+                    }
+                    else if (sugar == "Extra")
+                    {
+                        price = 1.2;
+                    }
+
+                    break;
+                    case "Cappuccino":
+                    if (sugar == "Without")
+                    {
+                        price = 1;
+                    }
+                    else if (sugar == "Normal")
+                    {
+                        price = 1.2;
+                    }
+                    else if (sugar == "Extra")
+                    {
+                        price = 1.6;
+                    }
+                    break;
+                    case "Tea":
+                    if (sugar == "Without")
+                    {
+                        price = 0.5;
+                    }
+                    else if (sugar == "Normal")
+                    {
+                        price = 0.6;
+                    }
+                    else if (sugar == "Extra")
+                    {
+                        price = 0.7;
+                    }
+                    break;
+
+
+
+
+                default:
+                    break;
+            }
+
+
+
+            if (sugar == "Without")
+            {
+                price = price * 0.65;
+            }
+
+            if (napitka == "Espresso" && num >= 5)
+            {
+                price = price * 0.75;
+            }
+
+
+            double sum = num * price;
+            if (sum > 15)
+            {
+                sum = sum * 0.8;
+            }
+
+
+            Console.WriteLine($"You bought {num} cups of {napitka} for {sum:f2} lv.");
+
+
+        }
+    }
+}
