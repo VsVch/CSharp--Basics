952e301c3f8a93698d23d7cecc2b021a57cc76f6
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-22 06:40:53 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/01. Pool Day/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/01. Pool Day/Program.cs
@@ -1,1 +1,35 @@
+﻿using System;
 
+namespace _01._Pool_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numPeople = int.Parse(Console.ReadLine());
+            double taksaEmtrance = double.Parse(Console.ReadLine());
+            double priceOneShezlong = double.Parse(Console.ReadLine());
+            double priceAmbrela = double.Parse(Console.ReadLine());
+
+            double price = numPeople * taksaEmtrance;
+            double numShezlong = Math.Ceiling(numPeople*0.75);
+            double allPriceShezlong = numShezlong * priceOneShezlong;
+            double numAmbrela = Math.Ceiling(numPeople*0.5);
+            double priceAllAmbrela = numAmbrela * priceAmbrela;
+            double sumALL = price + allPriceShezlong + priceAllAmbrela;
+            Console.WriteLine($"{sumALL:f2} lv.");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
