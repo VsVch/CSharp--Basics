1c6fdd13e57ad5893b2eb9b9d600609ef0634259
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-22 06:44:12 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/04. Club/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/04. Club/Program.cs
@@ -1,1 +1,55 @@
+﻿using System;
 
+namespace _04._Club
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double profit = double.Parse(Console.ReadLine());
+
+            double income = 0;
+            double sum = 0;
+            double price = 0;
+            string input = Console.ReadLine();
+            bool isT = false;
+            while (input != "Party!")
+            {
+                price = input.Length;
+                int num = int.Parse(Console.ReadLine());
+                sum = price * num;
+                if (sum % 2 != 0)
+                {
+                    sum = sum * 0.75;
+                }
+
+
+                income += sum;
+
+                if (income >= profit)
+                {
+                    Console.WriteLine($"Target acquired.");
+                    break;
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+            if (input == "Party!")
+            {
+                Console.WriteLine($"We need {(profit - income):f2} leva more.");
+                isT = true;
+
+            }
+
+
+            Console.WriteLine($"Club income - {income:f2} leva.");
+            if (isT)
+            {
+                return;
+            }
+
+        }
+    }
+}
