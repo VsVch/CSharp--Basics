32c2d94f06b4c76fa8107a0f4fd25541872b96bc
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-22 06:41:36 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/02. Family Trip/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 6 and 7 July 2019/02. Family Trip/Program.cs
@@ -1,1 +1,35 @@
+﻿using System;
 
+namespace _02._Family_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double budjet = double.Parse(Console.ReadLine());
+            int numNights = int.Parse(Console.ReadLine());
+            double priceForOneNight = double.Parse(Console.ReadLine());
+            int dopRaz = int.Parse(Console.ReadLine());
+
+            double discount = 0;
+            if (numNights>7)
+            {
+                discount = 5;
+            }
+
+            double allDOpRaz = budjet * dopRaz / 100;
+            double sum = numNights * priceForOneNight - numNights * priceForOneNight * discount / 100 + allDOpRaz;
+
+            if (budjet >= sum)
+            {
+                Console.WriteLine($"Ivanovi will be left with {(budjet-sum):f2} leva after vacation.");
+            }
+            else
+            {
+                Console.WriteLine($"{(sum-budjet):f2} leva needed.");
+            }
+
+        }
+    }
+}
