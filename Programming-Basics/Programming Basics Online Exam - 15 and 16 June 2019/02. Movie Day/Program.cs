e62bd2d9ecd42411efcc6212e9f9545a76c0e05c
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-24 08:28:50 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/02. Movie Day/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/02. Movie Day/Program.cs
@@ -1,1 +1,31 @@
+﻿using System;
 
+namespace _02._Movie_Day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int timeForPhotos = int.Parse(Console.ReadLine());
+            int numPhotos = int.Parse(Console.ReadLine());
+            int timeForOnePhoto = int.Parse(Console.ReadLine());
+
+            double timePrepare = timeForPhotos * 0.15;
+            double timeAllPhotos = numPhotos * timeForOnePhoto;
+            double all = timePrepare + timeAllPhotos;
+            if (timeForPhotos>=all)
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeForPhotos-all)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(all-timeForPhotos)} minutes."); 
+            }
+
+            
+
+
+        }
+    }
+}
