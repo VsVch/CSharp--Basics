3644bca21173f59a2f861aa8baeafd5f54890f9d
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-24 08:35:33 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/06. Movie Tickets/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/06. Movie Tickets/Program.cs
@@ -1,1 +1,47 @@
+﻿using System;
 
+namespace _06._Movie_Tickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a1 = int.Parse(Console.ReadLine());
+            int a2 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = a1; i <= a2-1; i++)
+            {
+
+                char letar = (char)i;
+
+
+                for (int j = 1; j <= n-1; j++)
+                {
+                    for (int k = 1; k <= (n/2-1); k++)
+
+
+
+
+
+
+                    {
+
+
+                       
+
+                        if (i%2==1 && (i+k+j)%2==1)
+                        {
+                            Console.WriteLine($"{letar}-{j}{k}{i}");
+                        }
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
