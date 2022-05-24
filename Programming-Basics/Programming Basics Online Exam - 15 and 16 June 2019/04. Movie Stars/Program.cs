5cec51481c1fff99a36103fe304db02305822b94
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-24 08:32:05 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/04. Movie Stars/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/04. Movie Stars/Program.cs
@@ -1,1 +1,56 @@
+﻿using System;
 
+namespace _04._Movie_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double budjet = double.Parse(Console.ReadLine());
+            double chekBudjet = budjet;
+
+            string nameAktor = Console.ReadLine();
+            double allActors = 0;
+            
+
+            while (nameAktor != "ACTION")
+            {
+
+                double payOneAktor = 0;
+
+                if (nameAktor.Length <= 15)
+                {
+                    payOneAktor = double.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    payOneAktor = 0.2 * chekBudjet;
+                }
+
+                allActors += payOneAktor;
+                chekBudjet -= payOneAktor;
+
+                if (allActors > budjet)
+                {
+                    Console.WriteLine($"We need {(allActors-budjet):f2} leva for our actors.");
+                    break;
+                }
+
+
+                nameAktor = Console.ReadLine();
+            }
+
+
+            if (nameAktor == "ACTION")
+            {
+                Console.WriteLine($"We are left with {(budjet-allActors):f2} leva.");
+            }
+
+
+
+
+
+        }
+    }
+}
