92720b37858e84b8345a41c275b10967f2e131ac
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-24 08:28:08 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/01. Movie Profit/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 15 and 16 June 2019/01. Movie Profit/Program.cs
@@ -1,1 +1,27 @@
+﻿using System;
 
+namespace _01._Movie_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string movie = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int tickets = int.Parse(Console.ReadLine());
+            double priceForTicket = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double priceForDay = tickets * priceForTicket;
+            double sumForAll = days * priceForDay;
+            double discount = sumForAll * percent / 100;
+            double profit = sumForAll - discount;
+            Console.WriteLine($"The profit from the movie {movie} is {profit:f2} lv.");
+
+
+
+
+        }
+    }
+}
