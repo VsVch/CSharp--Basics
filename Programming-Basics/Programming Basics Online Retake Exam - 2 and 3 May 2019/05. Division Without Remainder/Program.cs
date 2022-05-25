3c93712440850c22f1ce32f96b53ac91fa77bb0e
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-25 21:46:22 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Retake Exam - 2 and 3 May 2019/05. Division Without Remainder/Program.cs
+++ Programming-Basics/Programming Basics Online Retake Exam - 2 and 3 May 2019/05. Division Without Remainder/Program.cs
@@ -1,1 +1,55 @@
+﻿using System;
 
+namespace _05._Division_Without_Remainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double num = 0;
+            double num4 = 0;
+            double num3 = 0;
+            double num2 = 0;
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                num = double.Parse(Console.ReadLine());
+                sum++;
+
+                if (num%4==0)
+                {
+                    num4++;
+                    
+                }
+                if (num%3==0)
+                {
+                    num3++;
+                }
+                if (num%2==0)
+                {
+                    num2++;
+                }
+
+
+            }
+
+
+            double p1 = num2 / sum * 100;
+            double p2 = num3 / sum * 100;
+            double p3 = num4 / sum * 100;
+
+            Console.WriteLine($"{p1:f2}%");
+            Console.WriteLine($"{p2:f2}%");
+            Console.WriteLine($"{p3:f2}%");
+
+
+
+
+
+
+        }
+    }
+}
