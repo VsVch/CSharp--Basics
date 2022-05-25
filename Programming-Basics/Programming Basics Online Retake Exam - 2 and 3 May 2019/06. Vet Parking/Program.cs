48ddbff00083c19baf764c28db6a33db9848fefa
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-25 21:47:07 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Retake Exam - 2 and 3 May 2019/06. Vet Parking/Program.cs
+++ Programming-Basics/Programming Basics Online Retake Exam - 2 and 3 May 2019/06. Vet Parking/Program.cs
@@ -1,1 +1,54 @@
+﻿using System;
 
+namespace _06._Vet_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int days = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double sum = 0;
+            
+
+            for (int i = 1; i <= days; i++)
+            {
+                double curenSum = 0;
+                for (int j = 1; j <= hours; j++)
+                {
+                    
+                    if (i%2!=0&&j%2==0)
+                    {
+                        sum += 1.25;
+                        curenSum += 1.25;
+                    }
+                    else if (i % 2 == 0 && j % 2 != 0)
+                    {
+                        sum += 2.5;
+                        curenSum += 2.5;
+                    }
+                    else
+                    {
+                        sum += 1;
+                        curenSum += 1;
+                    }
+
+                }
+                Console.WriteLine($"Day: {i} - {curenSum:f2} leva");
+            }
+
+
+
+            // Console.WriteLine($"Day: {[i]} – { общата сума за деня} leva");
+
+            Console.WriteLine($"Total: {sum:f2} leva");
+
+
+
+
+
+
+        }
+    }
+}
