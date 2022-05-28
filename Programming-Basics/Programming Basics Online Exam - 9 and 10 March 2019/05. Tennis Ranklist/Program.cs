2e11e46ee7c9f344bccc5e25cded39cc5dde9575
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-28 18:37:41 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/05. Tennis Ranklist/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/05. Tennis Ranklist/Program.cs
@@ -1,1 +1,51 @@
+﻿using System;
 
+namespace _05._Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            int points1 = int.Parse(Console.ReadLine());
+            int points = 0;
+            double numW = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+
+                string etap = Console.ReadLine();
+
+                switch (etap)
+                {
+                    case "W":
+                        points += 2000;
+                        numW++;
+                        break;
+                    case "F":
+                        points += 1200;
+                        break;
+                    case "SF":
+                        points += 720;
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+
+            }
+
+
+            Console.WriteLine($"Final points: {points+points1}");
+            Console.WriteLine($"Average points: {((points / num) ):f0}");
+            Console.WriteLine($"{(numW/num*100):f2}%");
+
+
+
+
+        }
+    }
+}
