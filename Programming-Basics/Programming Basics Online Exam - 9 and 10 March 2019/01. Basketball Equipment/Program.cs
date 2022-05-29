5b93c2b9b10517451edfeadc8fc26e272d7a6825
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-29 21:09:08 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/01. Basketball Equipment/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/01. Basketball Equipment/Program.cs
@@ -1,1 +1,27 @@
+﻿using System;
 
+namespace _01._Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double yearsPay = double.Parse(Console.ReadLine());
+
+            double priceKecove = yearsPay - yearsPay * 0.4;
+            double ekip = priceKecove - priceKecove * 0.2;
+            double topka = ekip / 4;
+            double aksesoari = topka / 5;
+
+            double sum = yearsPay + priceKecove + ekip + topka + aksesoari;
+
+            Console.WriteLine($"{sum:f2}");
+
+
+
+
+
+        }
+    }
+}
