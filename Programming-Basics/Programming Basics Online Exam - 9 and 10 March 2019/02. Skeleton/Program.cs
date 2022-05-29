333b8f4b9e32b2f486845bedca69b54042e54b1b
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-29 21:10:37 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/02. Skeleton/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/02. Skeleton/Program.cs
@@ -1,1 +1,44 @@
+﻿using System;
 
+namespace _02._Skeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double min = double.Parse(Console.ReadLine());
+            double sec = double.Parse(Console.ReadLine());
+            double ulei = double.Parse(Console.ReadLine());
+            double secFor = double.Parse(Console.ReadLine());
+
+
+
+            double fullTime = ulei / 100;
+            fullTime = fullTime * secFor;
+            double zabavqne = ulei / 120.000;
+            fullTime = fullTime - zabavqne*2.5;
+
+            
+
+            double sum = min * 60.000 + sec;
+            
+
+            if (fullTime<= sum)
+            {
+                Console.WriteLine($"Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {fullTime:f3}.");
+            }
+
+            else
+            {
+                Console.WriteLine($"No, Marin failed! He was {(fullTime-sum):f3} second slower.");
+            }
+
+
+
+
+            
+        }
+    }
+}
