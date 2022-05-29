965e788cb645285614e07d7bd685a6c03e988ca7
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-29 21:13:14 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/04. Darts/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 9 and 10 March 2019/04. Darts/Program.cs
@@ -1,1 +1,65 @@
+﻿using System;
 
+namespace _04._Darts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+
+            string pole = Console.ReadLine();
+            int poimts = 0;
+            int numStrack = 0;
+            int numStrackWith = 0;
+            int restPoints = 301;
+
+            while (pole != "Retire")
+            {
+                int curentPoints = int.Parse(Console.ReadLine());
+                switch (pole)
+                {
+                    case "Single":
+                        poimts = curentPoints;
+                        break;
+                    case "Double":
+                        poimts = 2 * curentPoints;
+                        break;
+                    case "Triple":
+                        poimts =  3 * curentPoints;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                restPoints -= poimts;
+                if (restPoints>0)
+                {
+                    numStrack++;
+                }
+                else if (restPoints<0)
+                {
+                    restPoints = restPoints + poimts;
+                    numStrackWith++;
+                }
+                else
+                {
+                    numStrack++;
+                    Console.WriteLine($"{name} won the leg with {numStrack} shots.");
+                    break;
+                }
+
+                pole = Console.ReadLine();
+
+            }
+
+            if (pole == "Retire")
+            {
+                Console.WriteLine($"{name} retired after {numStrackWith} unsuccessful shots.");
+            }
+
+        }
+    }
+}
