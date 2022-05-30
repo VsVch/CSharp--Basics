bc348ca5263e7ef35b607073e27e70ffd569f17b
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-30 20:49:34 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Lunch/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Lunch/Program.cs
@@ -1,1 +1,26 @@
+﻿using System;
 
+namespace _01._Easter_Lunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kozunaci = int.Parse(Console.ReadLine());
+            int koriQica = int.Parse(Console.ReadLine());
+            int kgKurabii = int.Parse(Console.ReadLine());
+
+            double sum = kozunaci * 3.2 + 4.35 * koriQica + kgKurabii * 5.4 + 0.15*12*koriQica;
+            Console.WriteLine($"{sum:f2}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
