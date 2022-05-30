f30f7e4f4c7eaf31f307e627d6a0e89a284ec34e
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-05-30 20:50:25 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/02. Easter Party/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/02. Easter Party/Program.cs
@@ -1,1 +1,45 @@
+﻿using System;
 
+namespace _02._Easter_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numGusts = int.Parse(Console.ReadLine());
+            double cuvertForOne = double.Parse(Console.ReadLine());
+            double budjetDesi = double.Parse(Console.ReadLine());
+            budjetDesi = budjetDesi - budjetDesi * 0.1;
+            double discount = 0;
+            double sum = 0;
+            double raz = 0;
+
+            if (numGusts >= 10 && numGusts <= 15)
+            {
+                discount = 0.15;
+            }
+            else if (numGusts >15 && numGusts <= 20)
+            {
+                discount = 0.2;
+            }
+            else if (numGusts > 20)
+            {
+                discount = 0.25;
+            }
+
+            sum = numGusts * cuvertForOne - numGusts * cuvertForOne * discount;
+            raz = Math.Abs(budjetDesi-sum);
+            if (budjetDesi >= sum)
+            {
+                Console.WriteLine($"It is party time! {raz:f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"No party! {raz:f2} leva needed.");
+            }
+
+
+        }
+    }
+}
