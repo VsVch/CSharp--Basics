b43a337a0c6e52216029d0538f6f1ff7a2b27eca
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-01 18:56:47 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Bakery/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Bakery/Program.cs
@@ -1,1 +1,30 @@
+﻿using System;
 
+namespace _01._Easter_Bakery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double cenaKgBrashno = double.Parse(Console.ReadLine());
+            double kgBrashno = double.Parse(Console.ReadLine());
+            double kgZahar = double.Parse(Console.ReadLine());
+            int koriQica = int.Parse(Console.ReadLine());
+            int paketiMaq = int.Parse(Console.ReadLine());
+
+            double cenaZahar = cenaKgBrashno - cenaKgBrashno * 0.25;
+            double cenaQica = cenaKgBrashno + cenaKgBrashno * 0.1;
+            double cenaMaq = cenaZahar - cenaZahar * 0.8;
+
+            double sum = kgBrashno * cenaKgBrashno + kgZahar * cenaZahar + koriQica * cenaQica + paketiMaq * cenaMaq;
+            Console.WriteLine($"{sum:f2}");
+
+
+
+
+
+
+        }
+    }
+}
