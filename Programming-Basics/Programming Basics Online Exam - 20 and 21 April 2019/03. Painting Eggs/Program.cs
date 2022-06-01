6005fe56de95243b0502c78f77f31d8d35ed9318
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-01 18:58:56 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/03. Painting Eggs/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/03. Painting Eggs/Program.cs
@@ -1,1 +1,81 @@
+﻿using System;
 
+namespace _03._Painting_Eggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sizeEggs = Console.ReadLine();
+            string colourEggs = Console.ReadLine();
+            int numPartidi = int.Parse(Console.ReadLine());
+            double priceOne = 0;
+
+            switch (sizeEggs)
+            {
+
+                case "Large":
+                    switch (colourEggs)
+                    {
+                        case "Red":
+                            priceOne = 16;
+                            break;
+                        case "Green":
+                            priceOne = 12;
+                            break;
+                        case "Yellow":
+                            priceOne = 9;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                case "Medium":
+                    switch (colourEggs)
+                    {
+                        case "Red":
+                            priceOne = 13;
+                            break;
+                        case "Green":
+                            priceOne = 9;
+                            break;
+                        case "Yellow":
+                            priceOne = 7;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                case "Small":
+                    switch (colourEggs)
+                    {
+                        case "Red":
+                            priceOne = 9;
+                            break;
+                        case "Green":
+                            priceOne = 8;
+                            break;
+                        case "Yellow":
+                            priceOne = 5;
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+
+            double sum = numPartidi * priceOne;
+            sum = sum - sum * 0.35;
+            Console.WriteLine($"{sum:f2} leva.");
+
+
+        }
+    }
+}
