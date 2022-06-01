1e7c695be9acf8f6d83b36f15d28e98f57c53708
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-01 18:59:51 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/04. Easter Shop/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 20 and 21 April 2019/04. Easter Shop/Program.cs
@@ -1,1 +1,52 @@
+﻿using System;
 
+namespace _04._Easter_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstBeginningEggs = int.Parse(Console.ReadLine());
+            string imput = string.Empty;
+            
+            int sumBuyEggs = 0;
+            
+
+            while (imput != "Close")
+            {
+
+                
+                if (imput == "Buy" )
+                {
+                    int buyEggs = int.Parse(Console.ReadLine());
+                    if (buyEggs > firstBeginningEggs)
+                    {
+                        Console.WriteLine($"Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {firstBeginningEggs}.");
+                        break;
+                    }
+                    else
+                    {
+                        firstBeginningEggs = firstBeginningEggs - buyEggs;
+                        sumBuyEggs += buyEggs;
+                    }
+                    
+                }
+                else if (imput == "Fill")
+                {
+                    int fillEggs = int.Parse(Console.ReadLine());
+                    firstBeginningEggs += fillEggs;
+
+                }
+
+                imput = Console.ReadLine();
+            }
+            if (imput == "Close")
+            {
+                Console.WriteLine($"Store is closed!");
+                Console.WriteLine($"{sumBuyEggs} eggs sold.");
+            }
+        }
+    }
+}
