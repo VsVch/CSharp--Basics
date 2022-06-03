3587d4366db1b91a87114a0e6c5ca0f0bfeb73ce
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-03 20:20:37 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 18 and 19 July 2020/05. Best Player/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 18 and 19 July 2020/05. Best Player/Program.cs
@@ -1,1 +1,53 @@
+﻿using System;
 
+namespace _05._Best_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string bestPlayer = Console.ReadLine();
+            int bestGoal = 0;
+            string newBestPlayer = string.Empty;
+            int goals = 0;
+
+            while (bestPlayer != "END")
+            {
+                
+                goals = int.Parse(Console.ReadLine());
+                if (goals > bestGoal)
+                {
+                    bestGoal = goals;
+                    newBestPlayer = bestPlayer;
+                }
+
+                if (bestGoal >= 10)
+                {
+                    break;
+                }
+
+
+                bestPlayer = Console.ReadLine();
+            }
+
+
+            
+
+
+            Console.WriteLine($"{newBestPlayer} is the best player!");
+
+
+            if (bestGoal >= 3)
+            {
+                Console.WriteLine($"He has scored {bestGoal} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestGoal} goals.");
+            }
+
+
+        }
+    }
+}
