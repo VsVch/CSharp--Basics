3c67d3f9e26ada430a7b856d30208406483ab38b
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-03 20:17:17 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 18 and 19 July 2020/01. Agency Profit/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 18 and 19 July 2020/01. Agency Profit/Program.cs
@@ -1,1 +1,30 @@
+﻿using System;
 
+namespace _01._Agency_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int numAdult = int.Parse(Console.ReadLine());
+            int numKids = int.Parse(Console.ReadLine());
+            double netPriceAdult = double.Parse(Console.ReadLine());
+            double taxes = double.Parse(Console.ReadLine());
+
+            double netPriceKids = netPriceAdult - netPriceAdult * 0.7;
+            netPriceAdult = netPriceAdult + taxes;
+            netPriceKids = netPriceKids + taxes;
+            double sum = netPriceKids * numKids + netPriceAdult * numAdult;
+
+            double prfit = sum * 0.2;
+
+            Console.WriteLine($"The profit of your agency from {name} tickets is {prfit:f2} lv.");
+
+
+
+
+
+        }
+    }
+}
