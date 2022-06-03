b3e984032004bdb2928fe4b99f3466dbd9bb8f77
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-03 20:18:13 +0300

    Add files via upload

--- Programming-Basics/Programming Basics Online Exam - 18 and 19 July 2020/02. Add Bags/Program.cs
+++ Programming-Basics/Programming Basics Online Exam - 18 and 19 July 2020/02. Add Bags/Program.cs
@@ -1,1 +1,49 @@
+﻿using System;
 
+namespace _02._Add_Bags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            double priceMoreThen20 = double.Parse(Console.ReadLine());
+            double weight = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int numBagadjes = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (weight< 10)
+            {
+                price = priceMoreThen20 * 0.2;
+            }
+            else if (weight >=10 && weight <= 20)
+            {
+                price = priceMoreThen20 / 2;
+            }
+            else
+            {
+                price = priceMoreThen20;
+            }
+
+
+
+            if (days>30)
+            {
+                price = price + price * 0.1;
+            }
+            else if (days<=30 && days >= 7)
+            {
+                price = price + price * 0.15;
+            }
+            else if (days<7)
+            {
+                price = price + price * 0.4;
+            }
+            Console.WriteLine($"The total price of bags is: {numBagadjes*price:f2} lv.");
+        
+            
+        }
+    }
+}
