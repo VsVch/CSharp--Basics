ec40cee3ae98d807526c686d6d7047baa53ffe20
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-06-05 09:50:39 +0300

    Add files via upload

--- C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Minutes/Program.cs
+++ C# Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/04. Back In 30 Minutes/Program.cs
@@ -1,1 +1,25 @@
+﻿using System;
 
+namespace _04._Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+
+            min += 30;
+            if (min >= 60)
+            {
+                min = min - 60;
+                hours = hours + 1;
+                if (hours >=24)
+                {
+                    hours = hours - 24;
+                }
+            }
+            Console.WriteLine($"{hours}:{min:d2}");
+        }
+    }
+}
