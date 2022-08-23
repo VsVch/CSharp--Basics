fa512efbd2b4d88eeb65999fd1e63213bdaad7cf
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-08-23 19:54:53 +0300

    Add files via upload

--- Programming-Basics/Basic Syntax, Conditional Statements and Loops - 2/04. Reverse String/Program.cs
+++ Programming-Basics/Basic Syntax, Conditional Statements and Loops - 2/04. Reverse String/Program.cs
@@ -1,1 +1,22 @@
+﻿using System;
 
+namespace _04._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string reName = string.Empty;
+            for (int i = name.Length - 1; i >= 0 ; i--)
+            {
+                reName += name[i];
+            }
+            Console.WriteLine(reName);
+
+
+
+
+        }
+    }
+}
